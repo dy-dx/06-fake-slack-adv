37077ed333c9985c8dafe40a3051bdc796836c55
Chris Kim <christopherjkim@gmail.com>
2015-12-09 21:38:39 +0000

    added img and giphy support

--- public/js/script.js
+++ public/js/script.js
@@ -25,7 +25,47 @@ $('#submit-message').submit(function (e) {
   sendMessage(username, content);
 });
 
+var giphyCache = {};
+function giphySearch(term) {
+  if (giphyCache[term]) {
+    return giphyCache[term];
+  }
+  var img = '';
+  var giphyApiUrl = 'https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=';
+  $.ajax({
+    url: giphyApiUrl + term,
+    dataType: 'json',
+    async: false,
+    success: function(data) {
+      img = data['data'][0]['images']['fixed_width']['url'];
+    }
+  });
+  giphyCache[term] = img;
+  return img;
+}
+
 function formatMessage(message) {
+  var tokens = message.content.split(' ');
+  // check commands
+  var validCommands = ['/giphy'];
+  if (validCommands.indexOf(tokens[0]) >= 0) {
+    tokens[0] = giphySearch(tokens[1]);
+  }
+
+  // check for img url
+  var validExtensions = ['.gif', '.png', '.jpg'];
+  for (var i = 0; i < tokens.length; i++) {
+    var beginning = tokens[i].substr(0, 4);
+    var extension = tokens[i].substr(-4);
+    if (beginning === 'http' && (validExtensions.indexOf(extension) >= 0)) {
+      // replace token with <img> tag
+      var imgStr = '<img src="' + tokens[i] + '">';
+      tokens[i] = imgStr;
+    }
+  }
+  // join the tokens back together
+  message.content = tokens.join(' ');
+
   var timeString = new Date(message.timestamp).toLocaleTimeString();
   return '<li><div class="avatar"></div><span class="timestamp">' + timeString + '</span> ' + '<span class="username">' + message.user + '</span>' + ': ' + message.content + '</li>';
 }
@@ -44,4 +84,4 @@ function updateMessages() {
 
   getMessages(getMessagesCallback);
 }
-window.setInterval(updateMessages, 250);
+window.setInterval(updateMessages, 500);
